f57731898bdb98bf37f2a7982b5edff0a87b84c3
programLyrique <program@Lyrique.org>
2014-01-22 17:08:19 +0100

    Updated README.

--- README.md
+++ README.md
@@ -13,4 +13,18 @@ A tool to parse hadoop log files.
 
 ## Compilation
 
-make
+ * `make` : build *hadoopLogVisual*
+ * `grammar` : launch an interactive interpreter which takes tokens in input and checks whether there valid tokens of a Hadoop log file grammar
+* `tests` : unit tests
+
+
+## Usage
+
+### Output a formated json
+
+     hadoopLogVisual logFile
+
+### Output tokens 
+
+     hadoopLogVisual logFile d
+
--- hadoopLogVisual.ml
+++ hadoopLogVisual.ml
@@ -8,7 +8,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.*)
 
 
-(* Taken from the example in https://github.com/derdon/menhir-example/blob/master/calc.ml *)
+(* Adapted from the example in https://github.com/derdon/menhir-example/blob/master/calc.ml *)
 open Batteries
 
 
